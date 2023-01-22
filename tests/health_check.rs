84034a3c7f187dabfb197f3e865fcbb3e9fa5b84
Aarland <adrian.aarland@gmail.com>
2023-01-22 14:14:41 -0600

    Set up integration tests

---
+++ tests/health_check.rs
@@ -0,0 +1,33 @@
+//! tests/health_check.rs
+
+use std::net::TcpListener;
+
+use zero2prod;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+
+
+}
+
+fn spawn_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listner.local_addr().unwrap().port();
+    let server = zero2prod::run(listner).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
