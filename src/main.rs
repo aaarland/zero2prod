cdf4f113e4dcefaff57f22b366d0e9702be28402
Aarland <adrian.aarland@gmail.com>
2023-01-23 00:47:11 -0600

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 //! main.rs
 
 use std::net::TcpListener;
-
-use zero2prod::run;
-
+use zero2prod::startup::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     run(TcpListener::bind("127.0.0.1:8000").unwrap())?.await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,7 +6,7 @@ use zero2prod;
 fn spawn_app() -> String {
     let listner = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listner.local_addr().unwrap().port();
-    let server = zero2prod::run(listner).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listner).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
