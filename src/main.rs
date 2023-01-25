1621c91172fb6a3cbb52e0ada15e7bcb7371019e
Aarland <adrian.aarland@gmail.com>
2023-01-24 20:17:47 -0600

    Fixed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! main.rs
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::net::TcpListener;
 
 use zero2prod::{
@@ -15,9 +15,10 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to create postgres pool.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create postgres pool.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
