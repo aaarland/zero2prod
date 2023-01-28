6df0977d3317c095dd54fbd81867b62a817df56e
Aarland <adrian.aarland@gmail.com>
2023-01-27 21:26:05 -0600

    My app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! main.rs
 
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
@@ -17,8 +16,8 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(configuration.database.connection_string().expose_secret())
-        .expect("Failed to create postgres pool.");
+        .connect_lazy_with(configuration.database.with_db());
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,6 @@
 //! tests/health_check.rs
 
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Executor, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -45,14 +44,14 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let connection = PgPool::connect(&config.connection_string_without_db().expose_secret())
+    let connection = PgPool::connect_with(config.without_db())
         .await
         .expect("Failed to connect to Postgres.");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
